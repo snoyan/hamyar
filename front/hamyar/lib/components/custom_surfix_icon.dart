4941d251a1aac35981ff41ac7d9fcbaacdc6deeb
snoyan <epicteam1729@gmail.com>
2022-01-25 23:46:52 +0330

    SAM

--- front/hamyar/lib/components/custom_surfix_icon.dart
+++ front/hamyar/lib/components/custom_surfix_icon.dart
@@ -1,28 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
 class CustomSurffixIcon extends StatelessWidget {
-  String svgIcon;
-   Color color ;
-   CustomSurffixIcon({
-    required this.svgIcon ,
-     this.color =  Colors.black54,}
-      
-    
-  );
-
-   
+  final String svgIcon;
+  final Color color;
+  const CustomSurffixIcon({
+    Key? key,
+    required this.svgIcon,
+    this.color = Colors.black54,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.fromLTRB(
-        0,
-        20,
-        20,
-        20
-      ),
+      padding: EdgeInsets.fromLTRB(0, 20, 20, 20),
       child: SvgPicture.asset(
         svgIcon,
         height: 18,
