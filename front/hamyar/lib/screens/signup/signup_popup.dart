96cc3a73e35a674a0c6477d500a64e1a1087b414
samanmn325 <samanmn325@gmail.com>
2022-02-15 17:23:22 +0330

    loading for signup & login

--- front/hamyar/lib/screens/signup/signup_popup.dart
+++ front/hamyar/lib/screens/signup/signup_popup.dart
@@ -41,97 +41,110 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.08), // 4%
-                Text(
-                  "ثبت نام",
-                  style: TextStyle(
-                      fontFamily: "Iransans",
-                      //123456
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  "جهت ثبت نام اطلاعات زیر را تکمیل کرده و ثبت نام کنید!",
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.08),
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      buildEmailFormField(),
-                      SizedBox(height: 30),
-                      buildUsernameFormField(),
-                      SizedBox(height: 30),
-                      buildPasswordFormField(),
-                      SizedBox(height: 30),
-                      buildConformPassFormField(),
-                      FormError(errors: errors),
-                      SizedBox(height: 40),
-                      DefaultButton(
-                        text: "ثبت نام",
-                        press: () async {
-                          // if all are valid then go to success screen and then go to home screen
-                          if (_formKey.currentState!.validate()) {
-                            _formKey.currentState!.save();
-
-                            // Creates a new Woocommerce customer and returns the WooCustomer object.
-                            try {
-                              setState(() {
-                                showSpinner = true;
-                              });
-                              Network().creatUser(email!, userName!, password!);
-                              setState(() {
-                                showSpinner = false;
-                              });
-                              Navigator.pushNamedAndRemoveUntil(context,
-                                  NurseSignUp.routeName, (route) => false);
-                            } catch (e) {
-                              print(e);
-                            }
-                          }
-                        },
-                      ),
-                    ],
+    return WillPopScope(
+      onWillPop: () async => showSpinner == true ? false : true,
+      child: Scaffold(
+        body: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.08), // 4%
+                  Text(
+                    "ثبت نام",
+                    style: TextStyle(
+                        fontFamily: "Iransans",
+                        //123456
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
                   ),
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.08),
-                /* Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "جهت ثبت نام اطلاعات زیر را تکمیل کرده و ثبت نام کنید!",
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.08),
+                  Form(
+                    key: _formKey,
+                    child: Column(
                       children: [
-                        SocalCard(
-                          icon: "assets/icons/google-icon.svg",
-                          press: () {},
-                        ),
-                        SocalCard(
-                          icon: "assets/icons/facebook-2.svg",
-                          press: () {},
-                        ),
-                        SocalCard(
-                          icon: "assets/icons/twitter.svg",
-                          press: () {},
+                        buildEmailFormField(),
+                        SizedBox(height: 30),
+                        buildUsernameFormField(),
+                        SizedBox(height: 30),
+                        buildPasswordFormField(),
+                        SizedBox(height: 30),
+                        buildConformPassFormField(),
+                        FormError(errors: errors),
+                        SizedBox(height: 40),
+                        DefaultButton(
+                          text: "ثبت نام",
+                          press: () async {
+                            // if all are valid then go to success screen and then go to home screen
+                            if (_formKey.currentState!.validate()) {
+                              _formKey.currentState!.save();
+
+                              // Creates a new Woocommerce customer and returns the WooCustomer object.
+                              try {
+                                setState(() {
+                                  showSpinner = true;
+                                });
+                                Network()
+                                    .creatUser(email!, userName!, password!);
+                                setState(() {
+                                  showSpinner = false;
+                                });
+                                kShowToast(
+                                    context, 'ثبت نام با موفقیت انجام شد');
+
+                                Navigator.pushNamedAndRemoveUntil(context,
+                                    NurseSignUp.routeName, (route) => false);
+                              } catch (e) {
+                                setState(() {
+                                  showSpinner = false;
+                                });
+                                kShowToast(context,
+                                    'خطا در ثبت نام : \n هنگام ثبت نام کردن مشکلی به وجود آمده است ');
+
+                                print(e);
+                              }
+                            }
+                          },
                         ),
                       ],
-                    ),*/
-                SizedBox(height: 20),
-                Text(
-                  'ثبت نام شما به منزله ی  \تایید قوانین فروشگاه آلفا میباشد',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.caption,
-                )
-              ],
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.08),
+                  /* Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SocalCard(
+                            icon: "assets/icons/google-icon.svg",
+                            press: () {},
+                          ),
+                          SocalCard(
+                            icon: "assets/icons/facebook-2.svg",
+                            press: () {},
+                          ),
+                          SocalCard(
+                            icon: "assets/icons/twitter.svg",
+                            press: () {},
+                          ),
+                        ],
+                      ),*/
+                  SizedBox(height: 20),
+                  Text(
+                    'ثبت نام شما به منزله ی  \تایید قوانین فروشگاه آلفا میباشد',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.caption,
+                  )
+                ],
+              ),
             ),
           ),
         ),
